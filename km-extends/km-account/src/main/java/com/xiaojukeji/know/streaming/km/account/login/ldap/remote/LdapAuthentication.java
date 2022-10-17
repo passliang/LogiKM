dfcd1c541e9687a595e6b25e8000216859a94ef9
night.liang <night.liang@ponyft.com>
2022-10-17 22:30:52 +0800

    格式化处理

--- km-extends/km-account/src/main/java/com/xiaojukeji/know/streaming/km/account/login/ldap/remote/LdapAuthentication.java
+++ km-extends/km-account/src/main/java/com/xiaojukeji/know/streaming/km/account/login/ldap/remote/LdapAuthentication.java
@@ -27,139 +27,139 @@ import java.util.Hashtable;
  **/
 @Component
 public class LdapAuthentication {
-	private static final Logger LOGGER = LoggerFactory.getLogger(LdapAuthentication.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LdapAuthentication.class);
 
-	@Autowired
-	private KmAccountConfig kmAccountConfig;
+    @Autowired
+    private KmAccountConfig kmAccountConfig;
 
-	/**
-	 * LDAP账密验证
-	 */
-	public LdapPrincipal authenticate(String userName, String password) throws LogiSecurityException, LdapException {
-		// 获取ldap-context
-		LdapContext ctx = getLdapContext();
+    /**
+     * LDAP账密验证
+     */
+    public LdapPrincipal authenticate(String userName, String password) throws LogiSecurityException, LdapException {
+        // 获取ldap-context
+        LdapContext ctx = getLdapContext();
 
-		// 获取用户信息
-		LdapPrincipal ldapAttrsInfo = getLdapAttrsInfo(userName, ctx);
+        // 获取用户信息
+        LdapPrincipal ldapAttrsInfo = getLdapAttrsInfo(userName, ctx);
 
-		// 校验密码
-		try {
-			// 校验密码
-			ctx.addToEnvironment(Context.SECURITY_PRINCIPAL, ldapAttrsInfo.getUserDN());
-			ctx.addToEnvironment(Context.SECURITY_CREDENTIALS, password);
-			ctx.reconnect(null);
+        // 校验密码
+        try {
+            // 校验密码
+            ctx.addToEnvironment(Context.SECURITY_PRINCIPAL, ldapAttrsInfo.getUserDN());
+            ctx.addToEnvironment(Context.SECURITY_CREDENTIALS, password);
+            ctx.reconnect(null);
 
-			// 返回用户信息
-			return ldapAttrsInfo;
-		} catch (Exception e) {
-			LOGGER.error("method=authenticate||userName={}||errMsg={}", userName, e);
+            // 返回用户信息
+            return ldapAttrsInfo;
+        } catch (Exception e) {
+            LOGGER.error("method=authenticate||userName={}||errMsg={}", userName, e);
 
-			// 密码错误
-			throw new LogiSecurityException(ResultCode.USER_CREDENTIALS_ERROR);
-		} finally {
-			this.closeLdapContext(ctx);
-		}
-	}
+            // 密码错误
+            throw new LogiSecurityException(ResultCode.USER_CREDENTIALS_ERROR);
+        } finally {
+            this.closeLdapContext(ctx);
+        }
+    }
 
-	/**************************************************** private method ****************************************************/
+    /**************************************************** private method ****************************************************/
 
-	private LdapContext getLdapContext() throws LdapException {
-		Hashtable<String, String> env = new Hashtable<>();
+    private LdapContext getLdapContext() throws LdapException {
+        Hashtable<String, String> env = new Hashtable<>();
 
-		env.put(Context.INITIAL_CONTEXT_FACTORY, kmAccountConfig.getLdapFactory());
-		env.put(Context.PROVIDER_URL, kmAccountConfig.getLdapUrl() + kmAccountConfig.getLdapBaseDN());
-		env.put(Context.SECURITY_AUTHENTICATION, kmAccountConfig.getSecurityAuthentication());
+        env.put(Context.INITIAL_CONTEXT_FACTORY, kmAccountConfig.getLdapFactory());
+        env.put(Context.PROVIDER_URL, kmAccountConfig.getLdapUrl() + kmAccountConfig.getLdapBaseDN());
+        env.put(Context.SECURITY_AUTHENTICATION, kmAccountConfig.getSecurityAuthentication());
 
-		// 此处若不指定用户名和密码, 则自动转换为匿名登录
-		env.put(Context.SECURITY_PRINCIPAL, kmAccountConfig.getSecurityPrincipal());
-		env.put(Context.SECURITY_CREDENTIALS, kmAccountConfig.getSecurityCredentials());
-		try {
-			return new InitialLdapContext(env, null);
-		} catch (Exception e) {
-			LOGGER.error("method=getLdapContext||errMsg=exception", e);
+        // 此处若不指定用户名和密码, 则自动转换为匿名登录
+        env.put(Context.SECURITY_PRINCIPAL, kmAccountConfig.getSecurityPrincipal());
+        env.put(Context.SECURITY_CREDENTIALS, kmAccountConfig.getSecurityCredentials());
+        try {
+            return new InitialLdapContext(env, null);
+        } catch (Exception e) {
+            LOGGER.error("method=getLdapContext||errMsg=exception", e);
 
-			throw new LdapException("调用Ldap服务异常", e);
-		}
-	}
+            throw new LdapException("调用Ldap服务异常", e);
+        }
+    }
 
-	/**
-	 * 获取用户信息
-	 */
-	private LdapPrincipal getLdapAttrsInfo(String userName, LdapContext ctx) {
-		//存储更多的LDAP元信息
-		try {
-			SearchControls constraints = new SearchControls();
-			constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
+    /**
+     * 获取用户信息
+     */
+    private LdapPrincipal getLdapAttrsInfo(String userName, LdapContext ctx) {
+        //存储更多的LDAP元信息
+        try {
+            SearchControls constraints = new SearchControls();
+            constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
 
-			// 查找
-			NamingEnumeration<SearchResult> en = ctx.search("", "(&(objectClass=*)(" + kmAccountConfig.getLdapFilter() + "=" + userName + "))", constraints);
-			if (en == null || !en.hasMoreElements()) {
-				// 用户不存在
-				throw new LogiSecurityException(ResultCode.USER_NOT_EXISTS);
-			}
+            // 查找
+            NamingEnumeration<SearchResult> en = ctx.search("", "(&(objectClass=*)(" + kmAccountConfig.getLdapFilter() + "=" + userName + "))", constraints);
+            if (en == null || !en.hasMoreElements()) {
+                // 用户不存在
+                throw new LogiSecurityException(ResultCode.USER_NOT_EXISTS);
+            }
 
-			// maybe more than one element
-			while (en.hasMoreElements()) {
-				SearchResult obj = en.nextElement();
-				if (!ValidateUtils.isNull(obj)) {
-					// 携带LDAP更多元信息以填充用户元信息
-					LdapPrincipal ldapPrincipal = new LdapPrincipal();
-					ldapPrincipal.setUserDN(obj.getName() + "," + kmAccountConfig.getLdapBaseDN());
+            // maybe more than one element
+            while (en.hasMoreElements()) {
+                SearchResult obj = en.nextElement();
+                if (!ValidateUtils.isNull(obj)) {
+                    // 携带LDAP更多元信息以填充用户元信息
+                    LdapPrincipal ldapPrincipal = new LdapPrincipal();
+                    ldapPrincipal.setUserDN(obj.getName() + "," + kmAccountConfig.getLdapBaseDN());
 
-					Attributes attributes = obj.getAttributes();
-					//校验成功后 在获取值
-					if (!ValidateUtils.isNull(attributes)) {
-						ldapPrincipal.setSAMAccountName(getStringValueFromAttributes(attributes, "samaccountname"));
-						ldapPrincipal.setDepartment(getStringValueFromAttributes(attributes, "department"));
-						ldapPrincipal.setCompany(getStringValueFromAttributes(attributes, "company"));
-						ldapPrincipal.setDisplayName(getStringValueFromAttributes(attributes, "displayname"));
-						ldapPrincipal.setMail(getStringValueFromAttributes(attributes, "mail"));
-					}
-					return ldapPrincipal;
-				}
-			}
+                    Attributes attributes = obj.getAttributes();
+                    //校验成功后 在获取值
+                    if (!ValidateUtils.isNull(attributes)) {
+                        ldapPrincipal.setSAMAccountName(getStringValueFromAttributes(attributes, "samaccountname"));
+                        ldapPrincipal.setDepartment(getStringValueFromAttributes(attributes, "department"));
+                        ldapPrincipal.setCompany(getStringValueFromAttributes(attributes, "company"));
+                        ldapPrincipal.setDisplayName(getStringValueFromAttributes(attributes, "displayname"));
+                        ldapPrincipal.setMail(getStringValueFromAttributes(attributes, "mail"));
+                    }
+                    return ldapPrincipal;
+                }
+            }
 
-			// 用户不存在
-			throw new LogiSecurityException(ResultCode.USER_NOT_EXISTS);
-		} catch (Exception e) {
-			LOGGER.error("method=getLdapAttrsInfo||userName={}||errMsg=exception", userName, e);
+            // 用户不存在
+            throw new LogiSecurityException(ResultCode.USER_NOT_EXISTS);
+        } catch (Exception e) {
+            LOGGER.error("method=getLdapAttrsInfo||userName={}||errMsg=exception", userName, e);
 
-			throw new LdapException("调用Ldap服务异常", e);
-		}
-	}
+            throw new LdapException("调用Ldap服务异常", e);
+        }
+    }
 
-	private String getStringValueFromAttributes(Attributes attributes, String attrId) {
-		//增加 多重校验
-		int two = 2;
-		Attribute attribute = attributes.get(attrId);
-		if (ValidateUtils.isNull(attribute)) {
-			return "";
-		}
+    private String getStringValueFromAttributes(Attributes attributes, String attrId) {
+        //增加 多重校验
+        int two = 2;
+        Attribute attribute = attributes.get(attrId);
+        if (ValidateUtils.isNull(attribute)) {
+            return "";
+        }
 
-		String str = attribute.toString();
-		if (ValidateUtils.isBlank(str)) {
-			return "";
-		}
-		//分割字符串
-		String[] split = str.split(":\\s+");
-		if (ValidateUtils.isNotEmpty(split)) {
-			if (split.length >= two) {
-				return split[1];
-			}
-		}
+        String str = attribute.toString();
+        if (ValidateUtils.isBlank(str)) {
+            return "";
+        }
+        //分割字符串
+        String[] split = str.split(":\\s+");
+        if (ValidateUtils.isNotEmpty(split)) {
+            if (split.length >= two) {
+                return split[1];
+            }
+        }
 
-		return "";
-	}
+        return "";
+    }
 
-	private void closeLdapContext(LdapContext ctx) {
-		if (ctx == null) {
-			return;
-		}
+    private void closeLdapContext(LdapContext ctx) {
+        if (ctx == null) {
+            return;
+        }
 
-		try {
-			ctx.close();
-		} catch (Exception e) {
-			LOGGER.error("method=closeLdapContext||errMsg=exception", e);
-		}
-	}
+        try {
+            ctx.close();
+        } catch (Exception e) {
+            LOGGER.error("method=closeLdapContext||errMsg=exception", e);
+        }
+    }
 }
